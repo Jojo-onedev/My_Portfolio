ca78d9e52b66c590d6f65cc07df0696b9109cd70
Jojo-onedev <bationojonathan5@gmail.com>
2025-07-29 10:18:35 +0000

    deployement file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  server: {
+    historyApiFallback: true,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
+  preview: {
+    port: 3000,
+  },
 })
